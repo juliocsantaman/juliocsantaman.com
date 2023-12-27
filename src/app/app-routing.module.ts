e9104efb3ee7e2f32205d7d54e70449393ff5195
Julio Santaman <juliocsantaman@gmail.com>
2023-12-26 18:11:00 -0600

    Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutMainPageComponent } from './pages/layout-main-page/layout-main-page.component';
 import { LayoutMainPageModule } from './pages/layout-main-page/layout-main-page.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = 
 [
   {
     path: '',
-    component: LayoutMainPageComponent
+    component: LayoutMainPageComponent,
+    children: [
+      {
+        path: '',
+        component: HomePageComponent
+      }
+    ]
   }
 ];
 
