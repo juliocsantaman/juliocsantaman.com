52b832baabddda901f498d54431f8e738bbbf111
Julio Santaman <juliocsantaman@gmail.com>
2024-01-04 10:29:43 -0600

    Header
    Active link.

--- src/app/shared/organisms/header-organism/header-organism.component.ts
+++ src/app/shared/organisms/header-organism/header-organism.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IsActiveMatchOptions, Router, UrlTree } from '@angular/router';
 
 @Component({
   selector: 'app-header-organism',
   templateUrl: './header-organism.component.html',
   styleUrls: ['./header-organism.component.scss']
 })
-export class HeaderOrganismComponent {
+export class HeaderOrganismComponent implements OnInit {
 
+  constructor
+  (
+    private router: Router
+  ) 
+  {
+    
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  isLinkActive(route: string): boolean {
+    const urlTree: UrlTree = this.router.createUrlTree([route]);
+    const matchOptions: IsActiveMatchOptions = {
+      paths: 'exact', queryParams: 'exact', fragment: 'ignored',
+      matrixParams: 'ignored'
+    };
+
+    return this.router.isActive(urlTree, matchOptions);
+  }
 }
