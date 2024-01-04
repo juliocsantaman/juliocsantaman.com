192f684296615936c79d887e718f2afecadcc024
Julio Santaman <juliocsantaman@gmail.com>
2024-01-04 09:53:34 -0600

    Contact labels

--- src/app/shared/templates/contact-template/contact-template.module.ts
+++ src/app/shared/templates/contact-template/contact-template.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactTemplateComponent } from './contact-template.component';
-
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import { ContactTemplateComponent } from './contact-template.component';
     ContactTemplateComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ],
   exports: [
     ContactTemplateComponent
