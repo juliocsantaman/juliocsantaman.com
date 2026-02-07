66a37bf0a5181f5215d0e13012ed961f3bedbca1
Julio Cesar Santaman Cruz <juliocsantaman@gmail.com>
2026-02-07 17:15:51 -0500

    Creating component technologies, adding font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,16 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        colors: {
+          chart: {
+            "1": "hsl(var(--chart-1) / <alpha-value>)",
+            "2": "hsl(var(--chart-2) / <alpha-value>)",
+            "3": "hsl(var(--chart-3) / <alpha-value>)",
+            "4": "hsl(var(--chart-4) / <alpha-value>)",
+            "5": "hsl(var(--chart-5) / <alpha-value>)",
+          },
+        }
+
       },
 
       borderRadius: {
